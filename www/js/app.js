265be663bce178edd6a0d44a812c27102f9bf3bb
Divyaganapathy <gdivya100@gmail.com>
2016-06-27 19:47:48 +0530

    deleted playlist

--- www/js/app.js
+++ www/js/app.js
@@ -84,28 +84,7 @@ angular.module('tactics', ['ionic', 'tactics.controllers'])
                   templateUrl: 'templates/marketingMix.html'
                 }
               }
-            })
-
-
-    .state('app.playlists', {
-      url: '/playlists',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/playlists.html',
-          controller: 'PlaylistsCtrl'
-        }
-      }
-    })
-
-  .state('app.single', {
-    url: '/playlists/:playlistId',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/playlist.html',
-        controller: 'PlaylistCtrl'
-      }
-    }
-  });
+            });
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/app/home');
 });
